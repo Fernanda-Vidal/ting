4499b0296cab9718deadb017aea599445eb25eba
Fernanda Vidal <fevidal.dev@gmail.com>
2023-03-11 16:19:17 -0300

    feat: Requisito 3.

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,20 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.file_management import txt_importer
+from ting_file_management.queue import Queue
+import sys
+
+
+def process(path_file, instance: Queue):
+    for item in range(len(instance)):
+        if instance.search(item)['nome_do_arquivo'] == path_file:
+            return None
+    txt = txt_importer(path_file=path_file)
+    dict = {
+        'nome_do_arquivo': path_file,
+        'qtd_linhas': len(txt),
+        'linhas_do_arquivo': txt,
+    }
+    instance.enqueue(dict)
+    return sys.stdout.write(str(dict))
 
 
 def remove(instance):
