14bbf6aaac6cc3a9d5c11fef20b9adaaa46b119a
Fernanda Vidal <fevidal.dev@gmail.com>
2023-03-11 14:57:05 -0300

    feat: Requisito 2.

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
+from sys import stderr
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith('.txt'):
+        print("Formato Inválido", file=stderr)
+
+    try:
+        with open(path_file, mode="r") as file:
+            return file.read().split('\n')
+
+    except FileNotFoundError:
+        print(f"Arquivo{path_file} não encontrado", file=stderr)
